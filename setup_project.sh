93b8c639af67d92083b24dbd6bf3bc525f92403c
Bilgin COSKUN <bilgin.coskun@ceng.metu.edu.tr>
2019-09-08 16:41:47 +0300

    Keep Brogue Files and Symlink Them for Easier Build of Different Versions

--- setup_project.sh
+++ setup_project.sh
@@ -13,16 +13,17 @@ echo "Downloading SDL2_ttf"
 wget -qO- https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.tar.gz | tar xz
 mv SDL2_ttf* SDL_ttf
 
-echo "Downloading Brouge Files"
-#You can also use original distribution but dont forget to move necessary .h files and .c files to header and src/brogue folders
-git clone https://github.com/bilgincoskun/brogue-libtcod-sdl2-fix brogue-files -q
-
-
 mv SDL/android-project/app/src/main/java/org/libsdl "$cur_dir"/app/src/main/java/org/
 mv SDL "$cur_dir"/app/jni
 mv SDL_ttf "$cur_dir"/app/jni
-mv brogue-files/include "$cur_dir"/app/jni/src
-mv brogue-files/src/brogue "$cur_dir"/app/jni/src
-mv brogue-files/src/platform/platformdependent.c  "$cur_dir"/app/jni/src/brogue
+
 cd -
 rm -rf "$tmp_dir"
+
+echo "Downloading Brogue Files"
+#You can also use original distribution but dont forget to move necessary .h files and .c files to header and src/brogue folders
+git clone https://github.com/bilgincoskun/brogue-libtcod-sdl2-fix brogue-files -q
+
+echo "Setting Symlinks for Brogue Code"
+ln -sf ../../../brogue-files/include ./app/jni/src
+ln -sf ../../../brogue-files/src/brogue ./app/jni/src
